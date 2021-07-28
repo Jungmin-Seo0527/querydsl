35eadad2e198675df46d58b434b2495e2e12d861
Jungmin-Seo0527 <soato1405@naver.com>
2021-07-28 17:12:51 +0900

    3-1. 시작 - JPQL vs Querydsl

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,12 +1,3 @@
-# Querydsl
-
-## 3. 기본 문법
-
-### 3-1. 시작 - JPQL vs Querydsl
-
-#### QueryDslBasicTest.java - 테스트 기본 코드
-
-```java
 package study.querydsl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -78,15 +69,3 @@ public class QuerydslBasicTest {
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
 }
-
-```
-
-* `EntityManager`로 `JPAQueryFactory`생성
-* Querydsl은 JPQL 빌더
-* JPQL: 문자(실행 시점 오류), Querydsl: 코드(컴파일 시점 오류)
-* JPQL: 파라미터 바인딩 직접, Querydsl: 파라미터 바인딩 자동 처리
-* JPAQueryFactory를 필드로 제공하면 동시성 문제는 어떻게 될까?
-    * 동시성 문제는 JPAQueryFactory를 생성할 때 제동하는 `EntityManager(em)`에 달려있다.
-    * 스프링 프레임워크는 여러 쓰레드에서 동시에 같은 EntityManager에 접근해도, 트랜젝션 마다 별도의 영속성 컨텍스트를 제공하기 때문에, 동시성 문제는 걱정하지 않아도 된다.
-
-## Note
