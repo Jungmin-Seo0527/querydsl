8bd4d00c3631924eff029bd582b9be9070775312
Jungmin-Seo0527 <soato1405@naver.com>
2021-08-01 15:29:25 +0900

    5-1. 순수 JPA 리포지토리와 Querydsl

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -1,19 +1,3 @@
-# Querydsl
-
-## 5. 실무 활용 - 순수 JPA와 Querydsl
-
-* 순수 JPA 리포지토리와 Querydsl
-* 동적쿼리 Builder 적용
-* 동적쿼리 Where 적용
-* 조회 API 컨트롤러 개발
-
-### 5-1. 순수 JPA 리포지토리와 Querydsl
-
-#### MemberJpaRepository.java - 순수 JPA 리포지토리
-
-* `src/main/java/study/querydsl/repository/MemberJpaRepository.java`
-
-```java
 package study.querydsl.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -70,21 +54,3 @@ public class MemberJpaRepository {
                 .fetch();
     }
 }
-
-```
-
-#### JPQQueryFactory 스피링 빈 등록
-
-다음과 같이 `JPAQueryFactory`를 스프링 빈으로 등록해서 주입받아 사용해도 된다.
-
-```
-@Bean
-JPAQueryFactory jpaQueryFactory(EntityManager em) {
-        return new JPAQueryFactgory(em);
-}
-```
-
-> 참고: 동시성 문제는 걱정하지 않아도 된다. 왜냐하면 여기서 스프링이 주입해주는 엔티티 매니저는 실제 동작 시점에 진짜 엔티티 매니저를 찾아주는 프록시용 가짜 엔티티 매니저이다.       
-> 이 가짜 엔티티 매니저는 실제 사용 시점에 트랜잭션 단위로 실제 엔티티 매니져(영속성 컨텍스트)를 할당해준다.
-
-## Note
---
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,54 @@
+package study.querydsl.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired EntityManager em;
+    @Autowired MemberJpaRepository memberJpaRepository;
+
+    @Test
+    @DisplayName("리포지토리 테스트 - Querydsl 미사용")
+    public void basicTest() {
+        Member member = new Member("member1", 10);
+        memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.findById(member.getId()).orElseThrow();
+        assertThat(member).isEqualTo(findMember);
+
+        List<Member> result1 = memberJpaRepository.findAll();
+        assertThat(result1).containsExactly(member);
+
+        List<Member> result2 = memberJpaRepository.findByUsername("member1");
+        assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    @DisplayName("querydsl 적용 테스트")
+    public void basicQuerydslTest() {
+        Member member = new Member("member1", 10);
+        memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.findById(member.getId()).orElseThrow();
+        assertThat(member).isEqualTo(findMember);
+
+        List<Member> result1 = memberJpaRepository.findAll_Qeurydsl();
+        assertThat(result1).containsExactly(member);
+
+        List<Member> result2 = memberJpaRepository.findByUsername_Querydsl("member1");
+        assertThat(result2).containsExactly(member);
+    }
+}
